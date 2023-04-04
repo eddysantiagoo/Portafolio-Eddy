54bf227f7880ee1ec5a693f989da39ae8591add5
eddysantiagoo <eddysantiagogh@gmail.com>
2023-04-03 21:33:52 -0500

    Imagen de uso general en todo el codigo para la preview de la web

--- eddy.config.js
+++ eddy.config.js
@@ -5,9 +5,10 @@ module.exports = {
     githubName: 'eddysantiagoo',
     githubKey: process.env.GITHUB_KEY,
     email: 'eddysantiagogh@gmail.com',
+    imageWeb: 'https://i.ibb.co/F5tTMJF/el-sachi.png',
     messageContact: 'Al presionar "enviar" se ejecutara tu sistema de correos :)',
     errors: {
         404: "Esta pagina no ha sido encontrada :(",
-        500: "Ocurrio un error mientras procesabamos su solicitud :("
+        500: "Ocurrio un error mientras procesabamos tu solicitud :("
     }
 } 
