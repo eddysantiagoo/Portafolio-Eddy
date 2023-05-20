d9429dac7349cea0381b7532e03df26e46ca03d1
eddysantiagoo <eddysantiagogh@gmail.com>
2023-05-19 19:53:19 -0500

    🔍️ Actualizar version y imagen principal

--- eddy.config.js
+++ eddy.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     name: 'Eddy Santiago',
     navbar: 'EDDY',
-    version: '1.2', //Ultima revision el 4 de mayo del 2023
+    version: '1.3', //Ultima revision el 19 de mayo del 2023
     githubName: 'eddysantiagoo',
     githubKey: process.env.GITHUB_KEY,
     email: 'eddysantiagogh@gmail.com',
-    imageWeb: 'https://imgbox.io/ib/8OasTn0796.png',
+    imageWeb: 'https://eddy-portafolio.vercel.app/images/Proyecto-PortafolioPersonal.jpg',
     messageContact: 'Al presionar "enviar" se ejecutara tu sistema de correos',
     errors: {
         404: "Esta pagina no ha sido encontrada :(",
