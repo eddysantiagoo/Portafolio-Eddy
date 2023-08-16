5c87e7986ce84608bfbc3dd8eae9dc7696f9923b
Eddy Santiago <eddysantiagogh@gmail.com>
2023-08-16 13:16:03 -0500

    feat: bg not show

--- src/components/Global/background.jsx
+++ src/components/Global/background.jsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function Background() {
     return(
-       <div className="background-image-gradient h-screen w-screen md:bg-contain md:bg-center lg:bg-cover opacity-10"></div>
+       <div className="background-image-gradient h-screen w-screen md:bg-contain md:bg-center lg:bg-cover opacity-10" 
+            style={{ backgroundImage: 'url("/images/background-gradient.webp")'}}>
+       </div>
     )
 };
