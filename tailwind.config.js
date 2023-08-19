1891b028ea42c9be7d812401011d681d540076df
Eddy Santiago <eddysantiagogh@gmail.com>
2023-08-19 13:26:01 -0500

    fix: posts bad distribution

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,5 +25,5 @@ module.exports = {
     },
   },
   plugins: [require("@tailwindcss/typography")],
-  plugins: [require("tailwindcss-animated")],
+  // plugins: [require("tailwindcss-animated")],
 };
