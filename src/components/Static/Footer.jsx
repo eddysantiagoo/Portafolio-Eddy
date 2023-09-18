5079f4bd727196f6870f42c0168eb112870d116d
Eddy Santiago <eddysantiagogh@gmail.com>
2023-09-18 16:47:17 -0500

    feat: new colors to footer

--- src/components/Static/Footer.jsx
+++ src/components/Static/Footer.jsx
@@ -10,7 +10,7 @@ import 'animate.css'
 export default function Footer() {
     
 
-    let [heartColor, setHeartColor] = useState('#d89ffe');
+    let [heartColor, setHeartColor] = useState('#f83a3a');
 
     function randomColor() {
         let color = '#';
@@ -25,9 +25,9 @@ export default function Footer() {
     
         <div className="max-w-7xl mx-auto py-12 border-t border-gray-500/10">
             <div className="flex flex-col lg:flex-row items-center justify-between gap-4">
-                <p className="text-lg text-zinc-400 text-center">© {new Date().getFullYear()} {eddyConfig.name}. <p className="font-medium text-center"><a href="https://github.com/eddysantiagoo/Portafolio-Eddy/blob/main/thank%20u.md" target="_blank" rel="noopener noreferrer" className="underline text-[#d89ffe] hover:text-white">Construido con la ayuda de muchas personas y sus contribuciones</a></p> </p>
+                <p className="text-lg text-zinc-400 text-center">© {new Date().getFullYear()} {eddyConfig.name}. <p className="font-medium text-center"><a href="https://github.com/eddysantiagoo/Portafolio-Eddy/blob/main/thank%20u.md" target="_blank" rel="noopener noreferrer" className="underline text-zinc-200 hover:text-[#f83a3a]">Construido con la ayuda de muchas personas y sus contribuciones</a></p> </p>
                 <div className="text-right">
-                    <p className="font-medium text-zinc-400 select-none">Adaptado con <i onClick={() => { setHeartColor(randomColor()) }} className="fas fa-heart animate__animated animate__heartBeat animate__infinite"  style={{ color: heartColor, cursor: 'pointer' } } /> por <a href="https://youtu.be/dQw4w9WgXcQ" target={"_blank"} className="text-[#d89ffe] hover:text-primary hover:dark:text-white">Eddy</a></p>
+                    <p className="font-medium bg-gradient-to-r from-white to-[#7000ff] font-bold bg-clip-text text-transparent select-none">Adaptado con <i onClick={() => { setHeartColor(randomColor()) }} className="fas fa-heart animate__animated animate__heartBeat animate__infinite"  style={{ color: heartColor, cursor: 'pointer' } } /> por <a href="https://youtu.be/dQw4w9WgXcQ" target={"_blank"} className="text-white hover:text-primary hover:dark:text-[#b782fa]">Eddy</a></p>
                </div>
             </div>
         </div>
