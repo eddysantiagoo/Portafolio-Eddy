08645be29556ed4357a74ca93ced079e5cc2ddf6
Eddy Santiago <eddysantiagogh@gmail.com>
2024-01-18 11:00:16 -0500

    feat: remove unused resources:

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -6,12 +6,8 @@ export default function MyDocument() {
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="theme-color" content="#000" />
-                <link href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" rel="stylesheet" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
                 <link rel="shortcut icon" href="https://em-content.zobj.net/thumbs/160/whatsapp/273/bird_1f426.png" type="image/x-icon" />
-                <link href="https://api.fontshare.com/v2/css?f[]=technor@1,600,700&display=swap" rel="stylesheet"></link>
             </Head>
             <body>
                 <Main />
