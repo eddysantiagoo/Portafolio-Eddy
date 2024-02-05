76c91e59bbbb6310712b17801dc93e009e2a0031
Eddy Santiago <eddysantiagogh@gmail.com>
2024-02-05 12:58:43 -0500

    feat: add open graph image

--- eddy.config.js
+++ eddy.config.js
@@ -5,7 +5,7 @@ module.exports = {
     githubName: 'eddysantiagoo',
     githubKey: process.env.GITHUB_KEY,
     email: 'TEMPORY DISABLED',
-    imageWeb: 'https://eddy-portafolio.vercel.app/images/Proyecto-PortafolioPersonal.jpg',
+    imageWeb: 'https://eddy-portafolio.vercel.app/images/open-graph.jpg.jpg',
     messageContact: 'Al presionar "enviar" se ejecutara tu sistema de correos',
     errors: {
         404: "Esta pagina no ha sido encontrada :(",
