39b5844e4d3b279ee8e3b80dc5d670dc3cf81198
eddysantiagoo <eddysantiagogh@gmail.com>
2025-01-15 00:07:45 -0500

    chore: add v2 to portfolio web

--- src/components/Global/background.jsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Background() {
-    return(
-       <div className="background-image-gradient h-screen w-screen md:bg-contain md:bg-center lg:bg-cover opacity-10" 
-            style={{ backgroundImage: 'url("/images/background-gradient.webp")'}}>
-       </div>
-    )
-};
